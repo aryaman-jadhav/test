4cdb890bcd8814d4dea7d97679e2fb93e2f722ff
Aryaman Jadhav <aryaman@cs.utexas.edu>
2017-12-08 18:57:34 -0600

    removed

--- test.java
+++ test.java
@@ -5,10 +5,5 @@ public class test
 	System.out.println("This is a test");
     }
     
-    public static void newMethod()
-    {
-        System.out.println("something else");
-	System.out.println("This is new");
-    }
 
 }
